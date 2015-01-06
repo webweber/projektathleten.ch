6616eb422b121eec28750d1899ee07ba97d73188
Furqan Khanzada <furqan.khanzada@gmail.com>
2015-01-06 17:16:38 +0500

    bind click 1 time

--- js/projektathleten.js
+++ js/projektathleten.js
@@ -1,21 +1,17 @@
-// Wait for the page to load first
-window.onload = function() {
-	
-	
+// define global var
+// parse 1 time and cache for more usage
+var $body;
+
+// Wait for the page to ready first
+$(function(){
+	$body = $('body');
 	initializeStage();
-	
-	activateLinks();
-
-	//animateHeadline($('.cd-headline'));
-
-};
+});
 
 function initializeStage(){
-	$(".content").css("display","none");
-	displayContent('#cat_services');
-	
+	$body.find('.content').css('display', 'none');
+	displayContent('cat_services');
 	activateLinks();
-	//$("#cat_aervices .content").css("display","block");
 }
 
 function resetAnimate($headLine){
@@ -26,26 +22,16 @@ function resetAnimate($headLine){
 }
 
 function displayContent(id){
-	var $cat = $(id);
+	var $cat = $body.find('#'+ id);
 	resetAnimate($cat.find('.cd-headline'));
 	animateHeadline($cat.find('.cd-headline'));
-	$(".content").css("display", "none");
-	$cat.find(".content").css("display", "block");
+	$body.find('.content').css('display', 'none');
+	$cat.find('.content').css('display', 'block');
 }
 
 function activateLinks(){
-	var link_services = document.getElementById("link_services");
-	link_services.onclick = function(){
-	  displayContent("#cat_services");
-	}
-	var link_clients = document.getElementById("link_clients");
-	link_clients.onclick = function(){
-	  displayContent("#cat_clients");
-	}
-	var link_kontakt = document.getElementById("link_kontakt");
-	link_kontakt.onclick = function(){
-	  displayContent("#cat_kontakt");
-	}
+	$body.on('click', '.naviMainItem', function(e){
+		displayContent($(e.currentTarget).find('a').attr('name'));
+	});
 }
-
 
