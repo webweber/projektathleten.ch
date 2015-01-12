5aa2e3134a1f94dbb34db7eae691894b9dbcad98
Furqan Khanzada <furqan.khanzada@gmail.com>
2015-01-12 22:41:30 +0500

    routing has been done!

---
+++ js/functions.js
@@ -0,0 +1,48 @@
+function initializeStage($body){
+    $body.find('.content').css('display', 'none');
+    switchPage($body);
+    window.onpopstate = switchPage;
+}
+
+function activeLink(path, $body){
+    $body.
+        find('a[name="' + path + '"]').addClass('active')
+        .parent().siblings().find('a').removeClass('active');
+}
+
+function displayContent(path, $body){
+    var $page = $body.find('#'+ path);
+    if($page.filter(':not(.subcategory)').find('.content').is(':hidden')){
+        $body.find('.content').css('display', 'none');
+        $page.find('.content').first().css('display', 'block');
+        resetAnimate($page.find('.cd-headline'));
+        animateHeadline($page.find('.cd-headline'));
+    }else{
+        $body.find('.subcategory .content').css('display', 'none');
+        $page.find('.content').first().css('display', 'block');
+    }
+    activeLink(path, $body);
+}
+
+function resetAnimate($headLine){
+    $headLine.find('.cd-words-wrapper').width('auto').find('b')
+        .removeClass('is-hidden is-visible')
+        .filter(':eq(0)')
+        .addClass('is-visible');
+}
+
+function switchPage($body) {
+    var path = location.pathname,
+        pathArray = path.split('/');
+
+    if(!pathArray[pathArray.length - 1]){
+        location.pathname = location.pathname += 'projekte';
+        return;
+    }
+
+    for (var i = 0; pathArray.length > i; i++) {
+        if (pathArray[i]) {
+            displayContent(pathArray[i], $body.type ? $('body') : $body);
+        }
+    }
+}
--- js/projektathleten.js
+++ js/projektathleten.js
@@ -1,59 +1,18 @@
 // Wait for the page to ready first
-// define global var
-// parse 1 time and cache for more usage
-var $body;
 $(function(){
-	// define global var
 	// parse 1 time and cache for more usage
-	$body = $('body');
-	initializeStage();
-	$body.on('click', '.naviItem',function(e){
+	var $body = $('body');
+
+	// Initialize Website
+	initializeStage($body);
+
+	// Bind Click on all nav items
+	$body.on('click', '.naviItem', function(e){
 		e.preventDefault();
-		displayContent($(e.currentTarget).find('a').attr('name'));
 		var href = $(this).find('a').attr('href');
-		console.log(href);
 		// HISTORY.PUSHSTATE
 		history.pushState('', 'New URL: ' + href, href);
-		e.preventDefault();
-		console.log(href)
-
+		switchPage($body);
 	});
+
 });
-
-
-function initializeStage(){
-	$body.find('.content').css('display', 'none');
-	displayContent('cat_projects');
-
-
-}
-
-function activeLink(path){
-	$body.
-		find('a[name="' + path + '"]').addClass('active')
-		.parent().siblings().find('a').removeClass('active');
-}
-
-function displayContent(path){
-	var $page = $body.find('#'+ path);
-	if($page.filter(':not(.subcategory)').find('.content').is(':hidden')){
-		$body.find('.content').css('display', 'none');
-		$page.find('.content').first().css('display', 'block');
-		resetAnimate($page.find('.cd-headline'));
-		animateHeadline($page.find('.cd-headline'));
-	}else{
-		$body.find('.subcategory .content').css('display', 'none');
-		$page.find('.content').first().css('display', 'block');
-	}
-	activeLink(path);
-}
-
-function resetAnimate($headLine){
-	$headLine.find('.cd-words-wrapper').width('auto').find('b')
-		.removeClass('is-hidden is-visible')
-		.filter(':eq(0)')
-		.addClass('is-visible');
-}
-
-
-
